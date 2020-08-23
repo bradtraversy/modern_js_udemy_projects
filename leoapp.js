c7930565b1ad9763ddd94068fc5ff9a4de247eeb
Leo <rlalit@gmail.com>
2020-08-23 00:25:27 -0400

    Numbers and Math Operators Lesson

--- leoapp.js
+++ leoapp.js
@@ -1,1 +1,29 @@
-console.log("testing123");
+//NUMBERS AND MATH OPERATORS
+
+const num1 = 100;
+const num2 = 50;
+let val;
+
+//simple math with numbers
+val = num1+num2;
+val = num1*num2;
+val = num1-num2;
+val = num1%num2;
+
+//math objects
+val = Math.PI;
+val = Math.E;
+val = Math.round(2.8);
+val = Math.round(2.4);
+val = Math.ceil(2.3);//rounding it to the next one
+val = Math.floor(2.8);//rounding it down
+val = Math.sqrt(16);
+val = Math.abs(-2000);
+val = Math.pow(8,2);
+val = Math.min(1.243,121244,2214,211,1,0,-100);
+val = Math.max(12.4,31,2,1,2,-1000,111111);
+val = Math.random()
+val = Math.round(Math.random()*20);
+
+
+console.log(val);
