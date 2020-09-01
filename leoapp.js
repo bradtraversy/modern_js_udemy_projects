999710533f17e7596d49da3d5fc2b5f99cdd7227
Leo <rlalit@gmail.com>
2020-08-31 21:13:51 -0400

    Learned a couple of more chapters

--- leoapp.js
+++ leoapp.js
@@ -129,23 +129,130 @@
 
 */
 
-//ARRAYS
-//Create some arrays
-const numbers = [43,23,16,56,32,44,36];
-const numbers2 = new Array(22, 45,33,76,54); //another way to create arrays
+// //ARRAYS
+// //Create some arrays
+// const numbers = [43,23,16,56,32,44,36];
+// const numbers2 = new Array(22, 45,33,76,54); //another way to create arrays
 
-const fruit = ['APple','Banana','Orange','Pear'];
-const mixed = [22, 'Hello',true,undefined,null,{a:1, b:1},new Date()];
+// const fruit = ['APple','Banana','Orange','Pear'];
+// const mixed = [22, 'Hello',true,undefined,null,{a:1, b:1},new Date()];
 
-// console.log(mixed);
+// // console.log(mixed);
 
-let val;
+// let val;
 
-//Get array length
-val = numbers.length;
+// //Get array length
+// val = numbers.length;
 
-//Check if is array
-val = Array.isArray(numbers);
+// //Check if is array
+// val = Array.isArray(numbers);
 
-console.log(numbers);
-console.log(val);
+// //Get single value
+// val = numbers[3];
+// val = numbers[0];
+
+// //Insert into Array
+// // numbers[2] = 100;
+// //Find index of value
+// val = numbers.indexOf(36);
+
+// //MUTATING ARRAYS
+// //ADD ONTO END OF ARRAY - PUSH
+// // numbers.push(240);
+
+// // //Add onto beginning of array - unshift
+// // numbers.unshift(240);
+
+// // //Take off from end - POP
+// // numbers.pop(); //removes the 240 at the end
+
+// // //Take off from front
+// // numbers.shift();
+
+// // //Splice values
+// // numbers.splice(1,3);
+
+// // //Reverse the array
+// // numbers.reverse();
+
+// //Concatenate arrays
+// val = numbers.concat(numbers2);
+
+// //Sorting arrays
+// val = fruit.sort();
+// // val = numbers.sort();
+
+// // //Use the "compare function"
+// // val = numbers.sort(function(x,y){
+// //   return x-y;
+// // });
+
+// // //Reverse sort
+// // val = numbers.sort(function(x,y){
+// //   return y-x;
+// // });
+
+// //Find
+// // function under50(num){
+// //   return num <50;
+// // }
+
+// // val = numbers.find(under50);
+
+// function over50(num){
+//   return num > 50;
+// }
+
+// val = numbers.find(over50);
+
+// console.log(numbers);
+// console.log(val);
+
+
+// //OBJECT LITERALS REFERENCE TYPE
+
+// const person ={
+//   firstName:'Steve',
+//   lastName : 'Smith',
+//   age: 35,
+//   email: 'steve@aol.com',
+//   hobbies:['music','sports'],
+//   address:{
+//     city:'Miami',
+//     state: 'FL',
+//   },
+//   getBirthYear: function(){
+//     return 2020 - this.age;
+//   }
+// } //this defines an object literal (without any value) and we can place key value pairs inside it
+
+// let val;
+
+// val = person;
+// //Get specific value
+// val = person.firstName;
+// val = person['firstName']; //another way to get items from an object
+// val = person.age;
+// val = person.hobbies[1];
+// val = person.address;
+// val = person.address.state;
+// val = person.address['city'];
+// val = person.getBirthYear();
+
+
+// console.log(val);
+
+// const people = [
+//   {name:'John',age:30},
+//   {name:'Mike',age:23},
+//   {name:'Mike1',age:23}
+
+// ];
+
+// //FOR LOOP
+// for (let i =0; i<people.length; i++){
+//   console.log(people[i].name);
+// }
+
+//WORKING WITH DATES AND TIMES IN JAVASCRIPT
+
